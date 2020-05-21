6a655c80a20c2237720280e6e796a605b5919792
Trouni Tiet <trouni@gmail.com>
2020-05-21 21:46:11 +0900

    improved seeds and FE for base app

--- Gemfile
+++ Gemfile
@@ -29,6 +29,7 @@ gem 'turbolinks', '~> 5'
 gem 'bootsnap', '>= 1.4.2', require: false
 
 gem 'autoprefixer-rails'
+gem 'faker'
 gem 'font-awesome-sass'
 gem 'simple_form'
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,11 @@
+puts 'Destroying restaurants and reviews...'
+Restaurant.destroy_all
+
 puts 'Creating restaurants...'
-Restaurant.create!({
-  name: "Le Dindon en Laisse",
-  address: "18 Rue Beautreillis, 75004 Paris, France"
-})
-Restaurant.create!({
-  name: "Neuf et Voisins",
-  address: "Van Arteveldestraat 1, 1000 Brussels, Belgium"
-})
+5.times do
+  Restaurant.create!({
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address
+  })
+end
 puts 'Finished!'
